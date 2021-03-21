36aeb908902e94636b43a9bf98c8d6e3a38c39d4
DarkhorseHarshawDominic <32144593+DarkhorseHarshawDominic@users.noreply.github.com>
2021-03-21 17:26:09 -0400

    Gumby's delight

---
+++ placeToken.java
@@ -0,0 +1,11 @@
+import java.awt.Image;
+import java.awt.Point;
+
+public class placeToken {
+	public Point point = null;
+	public Image img = null;
+	public int hiddenValue = 0;
+	public placeToken(int x, int y) {
+		point = new Point(x, y);
+	}
+}
